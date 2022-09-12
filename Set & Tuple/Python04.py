e250a73ae500ec4de10b98dd6ab663d369fc1a4a
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-09-12 22:11:49 +0900

    계속해서 연습하는 Set & Tuple methods

---
+++ Set & Tuple/Python04.py
@@ -0,0 +1,11 @@
+# 튜플의 경우 메서드가 2가지 뿐이다.
+
+# count()의 사용법
+thisTuple = (1,3,3,2,4,4,5,6,5,6,6,62,3,3,4)
+x = thisTuple.count(6) # 리스트에서 사용하던 방식 그대로 사용하면 된다.
+print(x)
+
+# index()의 사용법
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = thistuple.index(5)
+print(x)
---
+++ Set & Tuple/Python05.py
@@ -0,0 +1,33 @@
+# Set의 메서드 Set은 순서와 중복을 허용하지 않는다. 그러므로 유니크한 값을 쉽게 알 수 있는 것이다.
+
+# add()의 사용법
+fruits = {"apple", "banana", "cherry"}
+fruits.add('orange') # 다른 리스트나 튜플의 add처럼 똑같이 추가된다.
+print(fruits)
+
+fruits = {"apple", "banana", "cherry"}
+fruits.add('apple') # 하지만 셋을 특성상 중복을 허용하지 않고 순서가 없기에 같은 요소를 또 추가할 수는 없다.
+print(fruits)
+
+# clear()의 사용법
+
+fruits = {"apple", "banana", "cherry"}
+fruits.clear() # 셋 안에 있는 요소들을 전부 클리어하는 메서드 입니다.
+print(fruits)
+
+# copy()의 사용법
+
+fruits = {"apple", "banana", "cherry"}
+x = fruits.copy() # 여기에서도 원본은 건드리지 않고 사용하기 위해서 copy를 사용한다.
+print(x)
+
+# difference()의 사용법
+x = {'apple', 'banana','cherry'}
+y = {'apple', 'microsoft', 'google'}
+
+z = x.difference(y) # difference()의 경우 다른 점을 리턴해준다. 즉 x안에 있는 요소 중 y에 있다면 그 요소를 제외하고 보여준다.
+print(z)
+
+
+
+
