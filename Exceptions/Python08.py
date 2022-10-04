0b0104e9a6e1c27b12e704e4c3b63a6f1234e798
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-10-05 08:10:42 +0900

    계속해서 연습하는 Python Keywords!

---
+++ Exceptions/Python08.py
@@ -0,0 +1,9 @@
+''' Python Exceptions 의 종류
+Total 33 Exceptions'''
+
+# ArithmeticError = Raise when an error occur in numberic calculations
+# AssertaionError = Raise when an assert statement fails -
+''' assert 는 True 인지 False 인지를 따지고 만약 False 이면 False 만들어낸 오류를 죽여버린다.'''
+# AttributeError = Raise when attribute reference and assignment fails
+# Exception = base class for all Exceptions
+# EOFError = Raise with input() method hits an "end of file" condition(EOF)
