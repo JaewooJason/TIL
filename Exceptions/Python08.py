c3e502665c56eea5383b0dd56c61aa824c4e148b
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-10-06 07:45:38 +0900

    계속해서 연습하는 Python Exception!

--- Exceptions/Python08.py
+++ Exceptions/Python08.py
@@ -7,3 +7,12 @@ Total 33 Exceptions'''
 # AttributeError = Raise when attribute reference and assignment fails
 # Exception = base class for all Exceptions
 # EOFError = Raise with input() method hits an "end of file" condition(EOF)
+# FloatingPointError = Raise when floating point calculation fails
+# GeneratorExit = Raise when generator is closed (with the close() method)
+# Importerror = Raise when an import module does not exist
+# IndentationError = Raise when indentation is not correct
+# IndexError = Raise when index of a sequence does not exist
+# KeyError = Raise when Key does not exist in dictionary
+# KeyboardInterruption = Raise when user presses Ctrl + c, Ctrl+z or delete
+# LockupError = Raise when errors raised cant be found
+
