1c63f233d1112cb16dd7bb63b914c187a89854dc
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-10-14 22:32:13 +0900

    계속해서 연습하는 Python Exception!

--- Basic/Function.py
+++ Basic/Function.py
@@ -62,6 +62,16 @@ def my_function(x):
     pass
 
 
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
 
 
 
