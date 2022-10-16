2578ea102e6da43f63afa0c22b0c14bc598ed7c7
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-10-16 22:36:29 +0900

    계속해서 연습하는 Python Exception!

--- Basic/Lambda.py
+++ Basic/Lambda.py
@@ -20,3 +20,20 @@ def myfunc(n):
 mydoubler = myfunc(2)
 
 print(mydoubler(11))
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mytripler = myfunc(3)
+
+print(mytripler(11))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
