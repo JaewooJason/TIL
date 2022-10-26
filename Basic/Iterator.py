6eeacf30fec966953307451192b834e80d6b0a01
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-10-26 20:39:46 +0900

    계속해서 연습하는 Python!

--- Basic/Iterator.py
+++ Basic/Iterator.py
@@ -43,3 +43,26 @@ print(next(myiter))
 print(next(myiter))
 print(next(myiter))
 print(next(myiter))
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
+
+# 아직 공부를 더 해봐야하는 파이썬 쉽게 이해가 가지 않는 iterator.......
+# iterable 은 for 을 생각하면 이해하기 쉽고 iterator은 다음에 연달아 나오게 하는 것이라고 생각하면 된다.
+# 또한 iterable 한 객체를 iterator하게 만들 수 있다!
