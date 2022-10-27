aebb40a3971f130a51969a12d0cb77bb53ae2909
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-10-28 08:28:44 +0900

    계속해서 연습하는 Python!

--- Basic/Try,,,,Except.py
+++ Basic/Try,,,,Except.py
@@ -26,3 +26,15 @@ try:
     f.close()
 except:
   print("Something went wrong when opening the file")
+
+
+# number에서도 사용가능
+x = -1
+
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
+
+x = "hello"
+
+if not type(x) is int:
+  raise TypeError("Only integers are allowed")
