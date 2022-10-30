b8446cf79de69b04ed44a1bbb3d174fa7acd682a
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-10-30 15:33:57 +0900

    계속해서 연습하는 Python!

--- Basic/Input.py
+++ Basic/Input.py
@@ -3,3 +3,27 @@ username = input('please enter your name')
 print('Username is ' + username)
 
 age = int(input()) # 이렇게 다양하게 사용할 수 있다.
+
+
+# formating 보통 모맷팅은 변수나 받아온 값을 자동으로 입력시켜주기 위해서 사용한다. 굳이 사용자 직접입력할 필요가 없음
+
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
+
+txt = "The price is {:.2f} dollars"
+
+# 이런식으로 하나만이 아님 다양하게 만들 수 있음
+price = 1000
+itemno = int(input())
+count = int(input())
+
+print(txt.format(price, itemno, count))
+
+
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
