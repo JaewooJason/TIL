a155057e716c91e6efa9bd1b343834997a59da8d
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-11-05 00:07:24 +0900

    계속해서 연습하는 Python!

--- Basic/RegEx.py
+++ Basic/RegEx.py
@@ -20,3 +20,12 @@ print(x)
 txt = "The rain in Paris"
 x = re.sub("\s", "o", txt)
 print(x)
+
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.string)
